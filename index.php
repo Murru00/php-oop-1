014ed8bfe84e3be48e74e01c32a8496345bccf5f
Murru00 <marcomurruolbia@gmail.com>
2024-03-09 14:30:30 +0100

    add class production

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+class Production
+{
+    public $title;
+    public $language;
+    public $vote;
+
+    function __construct($title, $language, $vote)
+    {
+        $this->title = $title;
+        $this->language = $language;
+        $this->vote = $vote;
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
