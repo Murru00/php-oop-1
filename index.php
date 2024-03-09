3f5c237550bb9acd5fc0eff5a0154556a4202f75
Murru00 <marcomurruolbia@gmail.com>
2024-03-09 14:54:32 +0100

    add film

--- index.php
+++ index.php
@@ -12,6 +12,31 @@ class Production
         $this->vote = $vote;
     }
 }
+
+$fast&furious = new Production(
+    "Fast&furiousX",
+    "Inglese",
+    "10/10"
+);
+
+$Avengers = new Production(
+    "Avengers",
+    "Inglese",
+    "8/10"
+);
+
+$Forrest_Gump= new Production(
+    "Forrest Gump",
+    "Inglese",
+    "5/10"
+);
+
+$productions = [
+    $fast&furious,
+    $Avengers,
+    $Forrest_Gump
+];
+var_dump($productions)
 ?>
 
 
@@ -22,6 +47,8 @@ class Production
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-oop-1</title>
+     <!-- BOOTS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
 </body>
